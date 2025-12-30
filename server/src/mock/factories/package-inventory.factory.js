c8b7d4058fc8c1fc8e8a691f9d7a63fcf43e5c95
KoinGame <obezet12@gmail.com>
2025-12-30 21:24:25 +0700

    .

--- server/src/mock/factories/package-inventory.factory.js
+++ server/src/mock/factories/package-inventory.factory.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker';
 export const createPackageInventory = (overrides = {}) => ({
   branch_id: overrides.branch_id ?? faker.number.int({ min: 1, max: 1000000 }),
   package_id: overrides.package_id ?? faker.number.int({ min: 1, max: 1000000 }),
-  quantity: overrides.quantity ?? faker.number.int({ min: 0, max: 1000 }),
+  quantity: overrides.quantity ?? faker.number.int({ min: 10000, max: 1000000 }),
 });
 
 export default (overrides = {}) => ({
--- server/src/mock/factories/vaccine-inventory.factory.js
+++ server/src/mock/factories/vaccine-inventory.factory.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker';
 export const createVaccineInventory = (overrides = {}) => ({
   branch_id: overrides.branch_id ?? faker.number.int({ min: 1, max: 1000000 }),
   vaccine_id: overrides.vaccine_id ?? faker.number.int({ min: 1, max: 1000000 }),
-  quantity: overrides.quantity ?? faker.number.int({ min: 0, max: 1000 }),
+  quantity: overrides.quantity ?? faker.number.int({ min: 10000, max: 1000000 }),
 });
 
 export default (overrides = {}) => ({
